51662d16356fdce5fc8bffaab9265fabcd6967c8
bnl <nbelob@users.noreply.github.com>
2020-05-26 14:31:47 +0300

    Creating the Cart

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { Product } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
+import { Cart } from "../model/cart.model";
+
 @Component({
   selector: "store",
   templateUrl: "store.component.html"
@@ -9,7 +11,10 @@ export class StoreComponent {
   public selectedCategory = null;
   public productsPerPage = 4;
   public selectedPage = 1;
-constructor(private repository: ProductRepository) {}
+
+constructor(private repository: ProductRepository,
+  private cart: Cart) { }
+
 get products(): Product[] {
   let pageIndex = (this.selectedPage - 1) * this.productsPerPage
   return this.repository.getProducts(this.selectedCategory)
@@ -42,4 +47,9 @@ get pageCount(): number {
 // .getProducts(this.selectedCategory).length / this.productsPerPage))
 // .fill(0).map((x, i) => i + 1);
 //}
+
+addProductToCart(product: Product) {
+  this.cart.addLine(product);
+}
+
 }
