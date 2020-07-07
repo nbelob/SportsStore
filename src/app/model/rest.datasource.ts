8eaa1481e7c03da696dd2c06138c914340e15852
bnl <nbelob@users.noreply.github.com>
2020-07-07 15:25:08 +0300

    Progressive Features and Deployment

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -16,7 +16,8 @@ export class RestDataSource {
   auth_token: string;
 
 constructor(private http: HttpClient) {
-  this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
+  //this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
+  this.baseUrl = "/api/"
 }
 getProducts(): Observable<Product[]> {
   return this.http.get<Product[]>(this.baseUrl + "products");
